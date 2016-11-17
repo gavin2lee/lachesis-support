c2583b61c996c41188ef7443e77fe5f9801cb6f6
gavin2lee <gavin2lee@163.com>
2016-11-17 19:04:32 +0800

    common init

---
+++ support-auth-common/src/main/java/com/lachesis/support/auth/common/vo/LocalUserVO.java
@@ -0,0 +1,65 @@
+package com.lachesis.support.auth.common.vo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class LocalUserVO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6400558355435304807L;
+
+	private String id;
+	private String username;
+	private String password;
+	private Collection<String> roles = new ArrayList<String>();
+	private Collection<String> permissions = new ArrayList<String>();
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<String> roles) {
+		if (roles != null) {
+			this.roles.clear();
+			this.roles.addAll(roles);
+		}
+	}
+
+	public Collection<String> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(Collection<String> permissions) {
+		if(permissions != null){
+			this.permissions.clear();
+			this.permissions.addAll(permissions);
+		}
+	}
+}
