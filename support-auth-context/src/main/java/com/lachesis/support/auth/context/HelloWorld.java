27426696fa698dc8a1ac797ed952fa738ea3b6a1
gavin2lee <gavin2lee@163.com>
2016-11-18 16:28:14 +0800

    restructure

---
+++ support-auth-context/src/main/java/com/lachesis/support/auth/context/HelloWorld.java
@@ -0,0 +1,5 @@
+package com.lachesis.support.auth.context;
+
+public class HelloWorld {
+
+}
---
+++ support-auth-context/src/test/java/com/lachesis/support/auth/context/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package com.lachesis.support.auth.context;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
