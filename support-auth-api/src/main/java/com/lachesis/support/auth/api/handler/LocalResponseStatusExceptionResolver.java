eb988b36f468ac3970dab81d443249cf68401df0
gavin2lee <gavin2lee@163.com>
2016-11-23 16:19:50 +0800

    log

--- support-auth-api/src/main/java/com/lachesis/support/auth/api/handler/LocalResponseStatusExceptionResolver.java
+++ support-auth-api/src/main/java/com/lachesis/support/auth/api/handler/LocalResponseStatusExceptionResolver.java
@@ -5,12 +5,15 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 
 import com.lachesis.support.auth.api.exception.AuthenticationException;
 
 public class LocalResponseStatusExceptionResolver extends AbstractHandlerExceptionResolver {
+	private static final Logger LOG = LoggerFactory.getLogger(LocalResponseStatusExceptionResolver.class);
 
 	@Override
 	protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
@@ -19,18 +22,21 @@ public class LocalResponseStatusExceptionResolver extends AbstractHandlerExcepti
 		try {
 			if (ex instanceof AuthenticationException) {
 				AuthenticationException authEx = (AuthenticationException) ex;
-				
+
 				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 				PrintWriter writer = response.getWriter();
 				writer.write(convertToJson(authEx));
 				writer.close();
-			}else{
+			} else {
 				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 				PrintWriter writer = response.getWriter();
 				writer.write(String.format("{\"error\":\"%s\"}", ex.getMessage()));
 				writer.close();
 			}
 		} catch (Exception handlerEx) {
+			if (LOG.isWarnEnabled()) {
+				LOG.warn("handler errors", handlerEx);
+			}
 		}
 
 		return new ModelAndView();
