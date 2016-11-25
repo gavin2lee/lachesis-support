935eb8ee2acf3687e446ae8cfc9c225e32631d00
gavin2lee <gavin2lee@163.com>
2016-11-25 16:50:39 +0800

    sql builder

--- support-auth-manager/src/main/java/com/lachesis/support/auth/common/SQLBuilder.java
+++ support-auth-manager/src/main/java/com/lachesis/support/auth/common/SQLBuilder.java
@@ -1,20 +1,53 @@
 package com.lachesis.support.auth.common;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.jdbc.SQL;
 
 public final class SQLBuilder {
+	public static final char UNDER_SCORE = '_';
 	
 	public String buildFindOneById(@Param("id") final Serializable id, @Param("clazz") final Class<?> clazz){
 		return new SQL() {{
-		    SELECT("ID as id");
-		    SELECT("CODE as code");
-		    SELECT("USERNAME as username");
-		    FROM("T_"+clazz.getSimpleName().toUpperCase());
-		    WHERE("ID = #{id}");
+			Field[] fs = clazz.getDeclaredFields();
+			
+			for(Field f : fs){
+				SELECT(build(f));
+			}
+			
+		    FROM(buildTableName(clazz));
+		    WHERE(buildWhere());
 		  }}.toString();
+	}
+	
+	private String buildWhere(){
+		return "ID = #{id}";
+	}
+	
+	private String buildTableName(Class<?> clazz){
+		return "T_"+clazz.getSimpleName().toUpperCase();
+	}
+	
+	private String build(Field f){
+		return String.format("%s as %s", buildCamelColumnName(f.getName()), f.getName());
+	}
+	
+	private String buildCamelColumnName(String fieldName){
+		StringBuilder sb = new StringBuilder();
+		int len = fieldName.length();
+		
+		for(int index=0; index < len; index++){
+			char c = fieldName.charAt(index);
+			if(Character.isUpperCase(c)){
+				sb.append(UNDER_SCORE).append(Character.toUpperCase(c));
+			}else{
+				sb.append(Character.toUpperCase(c));
+			}
+		}
+		
+		return sb.toString();
 	}
 	
 	public SQLBuilder(){}
