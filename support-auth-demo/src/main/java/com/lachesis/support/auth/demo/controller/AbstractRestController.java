5e7f4113004a936dcce46210a96b8f96daab2ce2
gavin2lee <gavin2lee@163.com>
2016-12-02 15:38:31 +0800

    add

--- support-auth-demo/src/main/java/com/lachesis/support/auth/demo/controller/AbstractRestController.java
+++ support-auth-demo/src/main/java/com/lachesis/support/auth/demo/controller/AbstractRestController.java
@@ -1,10 +1,17 @@
 package com.lachesis.support.auth.demo.controller;
 
-//@RequestMapping("/api/${support.auth.demo.version}")
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lachesis.support.auth.demo.vo.Book;
+import com.lachesis.support.vo.ResponseVO;
+
 public abstract class AbstractRestController {
 	
-//	@RequestMapping(value="/", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
-//	public ResponseVO createNew(@RequestBody Book book){
-//		return ResponseVO.ok(book);
-//	}
+	@RequestMapping(value="/", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseVO createNew(@RequestBody Book book){
+		return ResponseVO.ok(book);
+	}
 }
