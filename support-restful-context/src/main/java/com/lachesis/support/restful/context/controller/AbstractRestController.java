dae8419f9b009e4dcd122bd9e5edab92441dce46
gavin2lee <gavin2lee@163.com>
2016-12-06 18:53:04 +0800

    rest

--- support-restful-context/src/main/java/com/lachesis/support/restful/context/controller/AbstractRestController.java
+++ support-restful-context/src/main/java/com/lachesis/support/restful/context/controller/AbstractRestController.java
@@ -10,10 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.lachesis.support.restful.context.vo.RequestVO;
 import com.lachesis.support.restful.context.vo.ResponseVO;
 
-public abstract class AbstractRestController< V  extends RequestVO> {
+public abstract class AbstractRestController<V> {
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractRestController.class);
 	
 	@RequestMapping(method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
