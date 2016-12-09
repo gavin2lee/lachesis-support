1a2fd6660b5f564c2e33cadf3b35292e0e09a3bf
gavin2lee <gavin2lee@163.com>
2016-12-09 11:23:37 +0800

    role

--- support-restful-context/src/main/java/com/lachesis/support/restful/context/handler/LocalResponseStatusExceptionResolver.java
+++ support-restful-context/src/main/java/com/lachesis/support/restful/context/handler/LocalResponseStatusExceptionResolver.java
@@ -7,10 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authz.AuthorizationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
 
 public class LocalResponseStatusExceptionResolver extends AbstractHandlerExceptionResolver {
+	private static final Logger LOG = LoggerFactory.getLogger(LocalResponseStatusExceptionResolver.class);
 
 	@Override
 	protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
@@ -24,13 +27,13 @@ public class LocalResponseStatusExceptionResolver extends AbstractHandlerExcepti
 			} else {
 				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			}
-			
+
 			PrintWriter writer = response.getWriter();
 			writer.write(String.format("{\"msg\":\"%s\"}", ex.getMessage()));
 			writer.close();
 		} catch (Exception handlerEx) {
-			
-		} 
+			LOG.warn("errors occured in handler:" + handlerEx.getMessage());
+		}
 
 		return new ModelAndView();
 	}
