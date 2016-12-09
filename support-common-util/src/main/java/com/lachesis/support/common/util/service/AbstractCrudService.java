da54f612f1cbb27327800eb79cad20070636ab29
gavin2lee <gavin2lee@163.com>
2016-12-09 11:19:42 +0800

    base

--- support-common-util/src/main/java/com/lachesis/support/common/util/service/AbstractCrudService.java
+++ support-common-util/src/main/java/com/lachesis/support/common/util/service/AbstractCrudService.java
@@ -13,30 +13,78 @@ public abstract class AbstractCrudService<E> implements CrudService<E> {
 
 	@Transactional
 	public void addEntity(E entity) {
-		getBaseRepository().saveOne(entity, getEntityType());
+		if (entity == null) {
+			LOG.error("entity must be specified");
+			throw new IllegalArgumentException("entity must be specified");
+		}
+
+		Integer ret = getBaseRepository().saveOne(entity, getEntityType());
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(String.format("%s saved and return %d", entity.getClass().getName(), ret));
+		}
 	}
 
-	@Override
+	@Transactional
 	public void updateEntity(E entity) {
-		getBaseRepository().updateOne(entity, getEntityType());
+		if (entity == null) {
+			LOG.error("entity must be specified");
+			throw new IllegalArgumentException("entity must be specified");
+		}
+		Integer ret = getBaseRepository().updateOne(entity, getEntityType());
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(String.format("%s updated and return %d", entity.getClass().getName(), ret));
+		}
 	}
 
 	@Override
 	public E retrieveEntityById(Object id) {
-		return getBaseRepository().findOne(id, getEntityType());
+		if (id == null) {
+			LOG.error("id must be specified");
+			throw new IllegalArgumentException("id must be specified");
+		}
+		E entity = getBaseRepository().findOne(id, getEntityType());
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(String.format("%s retrieved.", entity == null ? "null" : entity.getClass().getName()));
+		}
+
+		return entity;
 	}
 
 	@Override
 	public List<E> retrieveEntitiesByCriteria(E criteria) {
-		return null;
+		if (criteria == null) {
+			LOG.error("criteria must be specified");
+			throw new IllegalArgumentException("criteria must be specified");
+		}
+
+		List<E> entities = getBaseRepository().findListByCriteria(criteria, getEntityType());
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(String.format("%s size %d retrieved", criteria.getClass().getName(),
+					(entities == null ? 0 : entities.size())));
+		}
+		return entities;
 	}
 
-	@Override
+	@Transactional
 	public void deleteEntity(Object id) {
-
+		if (id == null) {
+			LOG.error("id must be specified");
+			throw new IllegalArgumentException("id must be specified");
+		}
+		
+		Integer ret = getBaseRepository().deleteOne(id, getEntityType());
+		
+		if(LOG.isDebugEnabled()){
+			LOG.debug(String.format("%s deleted and return %d", getEntityType().getName(), ret));
+		}
 	}
-	
+
 	protected abstract Class<E> getEntityType();
+
 	protected abstract BaseRepository<E> getBaseRepository();
 
 }
