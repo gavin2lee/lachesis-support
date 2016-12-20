7e593c49a4e2dc784fb6eebcef3ace62167f34f2
gavin2lee <gavin2lee@163.com>
2016-12-20 15:23:06 +0800

    perm

--- support-auth-api/src/main/java/com/lachesis/support/auth/api/controller/PermissionController.java
+++ support-auth-api/src/main/java/com/lachesis/support/auth/api/controller/PermissionController.java
@@ -1,5 +1,62 @@
 package com.lachesis.support.auth.api.controller;
 
-public class PermissionController {
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lachesis.support.auth.data.PermissionService;
+import com.lachesis.support.objects.entity.auth.Permission;
+import com.lachesis.support.restful.context.vo.ResponseVO;
+
+@RestController
+@RequestMapping("permissions")
+public class PermissionController {
+	private static final Logger LOG = LoggerFactory.getLogger(PermissionController.class);
+
+	@Autowired
+	private PermissionService permService;
+
+	@RequestMapping(method = { RequestMethod.POST }, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseVO addPermission(@RequestBody Permission p) {
+		if(LOG.isTraceEnabled()){
+			LOG.trace("add permission:"+p);
+		}
+		Permission retPerm = permService.savePermission(p);
+		return ResponseVO.ok(retPerm);
+	}
+	
+	@RequestMapping(path = "/{id}", method = { RequestMethod.PUT }, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseVO modifyPermission(@PathVariable("id") long permId, @RequestBody Permission p){
+		if(LOG.isTraceEnabled()){
+			LOG.trace("modify permission:"+p);
+		}
+		Permission retPerm = permService.updatePermission(p);
+		return ResponseVO.ok(retPerm);
+	}
+	
+	@RequestMapping(method = { RequestMethod.GET }, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseVO getPermissions(){
+		List<Permission> perms = permService.findAll();
+		if(perms == null){
+			return ResponseVO.NOT_FOUND;
+		}
+		return ResponseVO.ok(perms);
+	}
+	
+	@RequestMapping(path = "/{id}", method = { RequestMethod.GET }, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseVO getPermission(@PathVariable("id") long permId){
+		Permission perm = permService.findPermissionById(permId);
+		if(perm == null){
+			return ResponseVO.NOT_FOUND;
+		}
+		return ResponseVO.ok(perm);
+	}
 }
--- support-auth-manager/src/main/java/com/lachesis/support/auth/data/PermissionService.java
+++ support-auth-manager/src/main/java/com/lachesis/support/auth/data/PermissionService.java
@@ -6,6 +6,7 @@ import com.lachesis.support.objects.entity.auth.Permission;
 
 public interface PermissionService {
 	Permission savePermission(Permission p);
+	Permission updatePermission(Permission p);
 	Permission removePermission(Permission p);
 	Permission findPermissionById(Long id);
 	Permission findPermissionByName(String name);
